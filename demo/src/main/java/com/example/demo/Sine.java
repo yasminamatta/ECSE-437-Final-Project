bd7c464972559385c528772e5f148bbfe5b8610b
Czarlhd <cesar-lahoud@hotmail.com>
2022-11-30 12:50:31 -0500

    removed uneccesary comments

--- demo/src/main/java/com/example/demo/Sine.java
+++ demo/src/main/java/com/example/demo/Sine.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
 import java.text.DecimalFormat;
-import com.example.demo.Cosine;
 
 public class Sine {
     private double n;
-    // static final double PI = 3.14159265359;
     DecimalFormat df = new DecimalFormat("#.########");
 
     public Sine(double n) {
@@ -13,18 +11,6 @@ public class Sine {
     }
 
     public String compute() {
-        // double x = this.n * (PI / 180.0);
-
-        // double res = x;
-        // double sign = 1, fact = 1,
-        // pow = 1;
-        // for (int i = 1; i < 5; i++) {
-        // sign = sign * -1;
-        // fact = fact * (2 * i) *
-        // (2 * i + 1);
-        // pow = pow * x * x;
-        // res = res + sign * pow / fact;
-        // }
         Cosine cosine = new Cosine(this.n);
         Double cos = Double.parseDouble(cosine.compute());
         Double res = Math.sqrt(1 - Math.pow(cos, 2));
