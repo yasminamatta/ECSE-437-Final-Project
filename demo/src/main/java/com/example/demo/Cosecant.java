0492e5ba49c02dc55a4a3dd85329842d2e34da1c
Czarlhd <cesar-lahoud@hotmail.com>
2022-12-01 22:13:24 -0500

    created cosecant function and test

--- demo/src/main/java/com/example/demo/Cosecant.java
+++ demo/src/main/java/com/example/demo/Cosecant.java
@@ -1,5 +1,22 @@
-// write cosecant class
 package com.example.demo;
 
+import java.text.DecimalFormat;
+
 public class Cosecant {
+
+    private double n;
+    DecimalFormat df = new DecimalFormat("#.######");
+
+    public Cosecant(double n) {
+        this.n = n;
+    }
+
+    public String compute() {
+
+        Sine sine = new Sine(this.n);
+        Double cosecant = 1 / Double.parseDouble(sine.compute());
+
+        return df.format(cosecant);
+    }
+
 }
