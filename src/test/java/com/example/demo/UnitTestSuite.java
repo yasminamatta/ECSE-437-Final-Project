3e0d62ddbb8e4ef982072089b30e0a7ed809d794
Czarlhd <cesar-lahoud@hotmail.com>
2022-12-04 14:55:07 -0500

    automated tests on build

--- src/test/java/com/example/demo/UnitTestSuite.java
+++ src/test/java/com/example/demo/UnitTestSuite.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-// import org.junit.platform*;
-
 import org.junit.platform.suite.api.*;
 
-@SuiteDisplayName("Test suite for the unit test")
+// @SuiteDisplayName("Test suite for the unit test")
+@SuiteDisplayName("Unit Test Suite")
 @SelectPackages({ "com.example.demo" })
 @Suite
 public class UnitTestSuite {
