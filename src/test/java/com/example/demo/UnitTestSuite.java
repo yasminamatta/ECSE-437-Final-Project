931138808a52c6c7ae7f1d8b21f345fd8a3edc77
Czarlhd <cesar-lahoud@hotmail.com>
2022-12-04 14:45:20 -0500

    created test suite

---
+++ src/test/java/com/example/demo/UnitTestSuite.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+// import org.junit.platform*;
+
+import org.junit.platform.suite.api.*;
+
+@SuiteDisplayName("Test suite for the unit test")
+@SelectPackages({ "com.example.demo" })
+@Suite
+public class UnitTestSuite {
+
+}
